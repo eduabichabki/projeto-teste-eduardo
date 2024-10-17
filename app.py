93406cab0e1a10f91be50c6b1d690a088163ad69
Eduardo Abichabki <edu.abichabki@gmail.com>
2024-10-17 11:03:38 -0300

    Versao 2

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 print("Ola, estamos no nosso primeiro projeto!")
+
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+calcular_preco_combo(30, 20)
+print("Programa finalizado!")
